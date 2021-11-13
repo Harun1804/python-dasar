2c2692c64b993d63eb0777807b8cc403563a6721
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-11-13 07:42:21 +0700

    Tipe Data

---
+++ 02 - Tipe Data/src/Main.py
@@ -0,0 +1,14 @@
+data_integer = 1
+data_float = 1.5
+data_string = "Ini String"
+data_bool = True
+
+## Tipe Data Khusus
+# Bilangan kompleks
+data_complex = complex(5,6)
+
+# Tipe data dari bahasa C
+from ctypes import c_double
+data_c_double = c_double(10.5)
+
+print(type*(data_integer)) # Mencari Tau Tipe Data
