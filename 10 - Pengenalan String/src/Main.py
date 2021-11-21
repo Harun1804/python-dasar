f181d39c536d2c3e1045f145b7b2e9fc1e4dec10
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-11-22 04:41:44 +0700

    Pengenalan String

---
+++ 10 - Pengenalan String/src/Main.py
@@ -0,0 +1,28 @@
+data = "ini adalah string"
+# 1. Cara membuat string
+'''
+    1. dengan menggunakan single quote '...'
+    2. dengan menggunakan double quote "..."
+'''
+
+data = 'menggunakan single quote'
+data = "menggunakan double quote"
+
+# 2. Dapat menggunakan \ untuk simbol
+print('mari sholat jum\'at')
+
+# 3. String literal atau raw
+print(r"C:\new folder")
+
+# Multiline literal string
+print("""
+Nama : X
+Kelas : X
+""")
+
+# Multiline literal string dan raw
+print(r"""
+Nama : X
+Kelas : X\S3 Oxford
+website : www.x.com/NewID
+""")
